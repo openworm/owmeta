8620cba5192e281796a3e7ac59b43384f4be46c4
Stephen Larson <stephen.larson@gmail.com>
2014-01-12 21:08:04 -0800

    fixing build

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,6 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
-import PyOpenWorm
 
 # -- General configuration -----------------------------------------------------
 
