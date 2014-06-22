9ef0f431c7d9d07ae37cb6caf0c441def083f06f
Stephen Larson <stephen.larson@gmail.com>
2014-06-22 11:28:39 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,16 @@ with open('requirements.txt') as f:
 long_description = open("README.md").read()
 
 setup(
-    #data_files=[("",['neuroml/test/Purk2M9s.nml'])],
     install_requires=required,
     name = "PyOpenWorm",
-    version = '0.0.1',
+    version = '0.0.1-alpha',
     packages = ['PyOpenWorm', 'tests'],
-    #package_data = {'neuroml.test': ['*.nml']},
     author = "OpenWorm.org authors and contributors",
     author_email = "info@openworm.org",
     description = "A Python library for working with OpenWorm data and models",
     long_description = long_description,
     license = "BSD",
-    url="http://PyOpenWorm.readthedocs.org/en/latest/",
+    url="http://github.com/openworm/pyopenworms",
     classifiers = [
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
