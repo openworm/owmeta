2c859f9cd14325cd90443cdfd0934b741877cfc4
Mark Watts <markw@cs.utexas.edu>
2014-07-07 01:34:11 -0500

    Moved more read/write functionality into Property and DataObject
    - Distinguished DatatypeProperties from ObjectProperties
    - Adjusted some tests

--- PyOpenWorm/connection.py
+++ PyOpenWorm/connection.py
@@ -8,9 +8,11 @@
 
 """
 import PyOpenWorm as P
-from PyOpenWorm import Relationship,Neuron,SimpleProperty
+from PyOpenWorm import *
 from string import Template
 import rdflib as R
+
+__all__ = ['Connection']
 
 class SynapseType:
     Chemical = "send"
@@ -25,24 +27,26 @@ class Connection(Relationship):
                  **kwargs):
         Relationship.__init__(self,**kwargs)
 
-        self.syntype = SimpleProperty(self,'syntype')
-        self.synclass = SimpleProperty(self,'neurotransmitter')
-        self.number = SimpleProperty(self,'number')
-        self.pre_cell = SimpleProperty(self,'pre')
-        self.post_cell = SimpleProperty(self,'post')
+        DatatypeProperty('syntype',owner=self)
+        DatatypeProperty('synclass',owner=self)
+        DatatypeProperty('number',owner=self)
+        ObjectProperty('pre_cell',owner=self)
+        ObjectProperty('post_cell',owner=self)
 
-        if isinstance(pre_cell,Neuron):
+        if isinstance(pre_cell,P.Neuron):
             self.pre_cell(pre_cell)
         elif pre_cell is not None:
-            self.pre_cell(Neuron(name=pre_cell, conf=self.conf))
+            self.pre_cell(P.Neuron(name=pre_cell, conf=self.conf))
 
-        if (isinstance(post_cell,Neuron)):
+        if (isinstance(post_cell,P.Neuron)):
             self.post_cell(post_cell)
         elif post_cell is not None:
-            self.post_cell(Neuron(name=post_cell, conf=self.conf))
+            self.post_cell(P.Neuron(name=post_cell, conf=self.conf))
 
         if isinstance(number,int):
             self.number(int(number))
+        elif number is not None:
+            raise Exception("Connection number must be an int, given %s" % number)
 
         if isinstance(syntype,basestring):
             syntype=syntype.lower()
@@ -52,14 +56,6 @@ class Connection(Relationship):
                 self.syntype(SynapseType.GapJunction)
         if isinstance(synclass,basestring):
             self.synclass(synclass)
-
-    def load(self):
-        qres = P.DataObject.load(self)
-        for r in qres:
-            try:
-                yield Connection(pre_cell=Neuron(ident=r['pre']), post_cell=Neuron(ident=r['post']), syntype=r['syntype'], number=r['number'])
-            except BaseException, e:
-                print e
 
     def __str__(self):
         return "Connection from %s to %s (%s times, type: %s, neurotransmitter: %s)"%(self.pre_cell, self.post_cell, self.number, self.syntype, self.synclass)
