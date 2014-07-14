b3174b3219484cb02eb750c7b83aca67320a34b2
Mark Watts <markw@cs.utexas.edu>
2014-07-13 21:30:35 -0500

    Passing some tests
    - Removed dependency on test-repo by using memory store. Gradually
      removing tests that depend on the repo being set up in advance
    
    
    Former-commit-id: 8cedbbe49e43ab310df12eafa59d735d3198d3b7 [formerly 5c2a30e7baa3d999f3e88f6fb10f2bb8f582593c] [formerly 4e7395b678079b2e3469d05083dea0325e2725a8] [formerly ba6a517db499e12eb707995a20a1fedf25290cba [formerly ba6a517db499e12eb707995a20a1fedf25290cba [formerly b53cdb426eef5b52f261e8a90732f3b2c70fb14a]]]
    Former-commit-id: ba6a517db499e12eb707995a20a1fedf25290cba
    Former-commit-id: b466a8ec65e763c21006edc4789ef8da9a498a02
    Former-commit-id: f838162e48ae797e321fa9ab70a3be5469e7ec44

--- PyOpenWorm/worm.py
+++ PyOpenWorm/worm.py
@@ -6,14 +6,16 @@
 
 """
 
-import PyOpenWorm
+import PyOpenWorm as P
 from PyOpenWorm import DataObject
 
 
 class Worm(DataObject):
 
-    def __init__(self,conf=False):
-        DataObject.__init__(self,conf)
+    def __init__(self,**kwargs):
+        DataObject.__init__(self,**kwargs)
+        P.ObjectProperty("neuron_network", owner=self)
+        P.ObjectProperty("muscle", owner=self)
 
     def get_neuron_network(self):
         """
@@ -21,8 +23,9 @@ class Worm(DataObject):
 
         :returns: An object to work with the network of the worm
         :rtype: PyOpenWorm.Network
-       """
-        return PyOpenWorm.Network(self.conf)
+        """
+        for x in self.neuron_network():
+            return x
 
     def muscles(self):
         """
@@ -31,25 +34,8 @@ class Worm(DataObject):
         :returns: A list of all muscle names
         :rtype: list
          """
-
-        qres = self['semantic_net_new'].query(
-                 """
-          SELECT ?subLabel     #we want to get out the labels associated with the objects
-            WHERE {
-              GRAPH ?g { #Each triple is in its own sub-graph to enable provenance
-                # match all subjects that have the 'is a' (1515) property pointing to 'muscle' (1519)
-                ?subject <http://openworm.org/entities/1515> <http://openworm.org/entities/1519> .
-                }
-              #Triples that have the label are in the main graph only
-              ?subject rdfs:label ?subLabel  #for the subject, look up their plain text label.
-            }
-          """)
-
-        muscles = []
-        for r in qres.result:
-            muscles.append(str(r[0]))
-
-        return muscles
+        for x in self.muscle():
+            yield x
 
     def get_semantic_net(self):
         """
