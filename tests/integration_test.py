45838ea8d5b9072ee98af76eb629ac330dbd6ffa
Mark Watts <markw@cs.utexas.edu>
2014-07-17 20:16:56 -0500

    Added a timing test
    - added a convenience, values, class for saving lots of objects
    - extended SimpleProperty identifier() generation to ObjectProperties
    
    
    Former-commit-id: 91b1c2445886d7980c893df9e7073cb953601d3c [formerly 91b1c2445886d7980c893df9e7073cb953601d3c [formerly 0082680f449d4f61de222776dd5877e46415a8f0]]
    Former-commit-id: 091ce126d387faf09b6155c3307975ed2d633140
    Former-commit-id: 24aaba65cf8e1998917fc8605ffcf0fd7ee969d2

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,4 +1,5 @@
 import unittest
+import traceback
 import rdflib as R
 from PyOpenWorm import *
 
@@ -22,6 +23,7 @@ class IntegrationTest(unittest.TestCase):
         c = Connection(pre_cell=n1,post_cell=n2,synclass="synapse")
         e = Evidence(bibtex=bt['white86'])
         e.asserts(c)
+        print list(e.load())
 
     def test_2(self):
         # Reference two neurons
@@ -59,6 +61,46 @@ class IntegrationTest(unittest.TestCase):
         c = Connection()
         for x in c.synclass():
             print x
+    def testl(self):
+        """
+        Test that a property can be loaded when the owning
+        object doesn't have any other values set
+        This test is for many objects of the same kind
+        """
+        from random import random
+        from time import time
+        from subprocess import call
+        import os
+        # Generate data sets from 10 to 10000 in size
+        #  query for properties
+        print 'starting testl'
+        class _to(DataObject):
+            def __init__(self,x=False):
+                DataObject.__init__(self)
+                DatatypeProperty('flexo',owner=self)
+                if x:
+                    self.flexo(x)
+        nums = [10, 1e2, 1e3, 1e4, 1e5]
+
+        Configureable.default = Data.open("tests/test_testl.conf")
+        Configureable.default.openDatabase()
+        try:
+            #for 1000, takes about 10 seconds...
+            for x in nums:
+                print 'running ',x,'sized test on a ',Configureable.default['rdf.graph'].store,'store'
+                v = values()
+                for z in range(int(x)):
+                    v.add(_to(random()))
+                t0 = time()
+                v.save()
+                for _ in _to().flexo():
+                    pass
+                t1 = time()
+                print "took", t1 - t0, "seconds"
+                Configureable.default['rdf.graph'].remove((None,None,None))
+        except:
+            traceback.print_exc()
+        Configureable.default.closeDatabase()
 
 # XXX: OTHER TESTS TO DO
 # reference a synaptic connection
