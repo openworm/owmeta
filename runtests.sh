f935b9e66341db0241efcf16ca5f41a17df4769a
Mark Watts <markw@cs.utexas.edu>
2014-07-17 20:16:56 -0500

    Added a timing test
    - added a convenience, values, class for saving lots of objects
    - extended SimpleProperty identifier() generation to ObjectProperties
    
    
    Former-commit-id: 43ca4c3f7b8f1f57bbce904c6527f0caa645b004 [formerly 45838ea8d5b9072ee98af76eb629ac330dbd6ffa] [formerly ef1fa21f6b6015dce7687c5d1f365f174d64da7e [formerly 24aaba65cf8e1998917fc8605ffcf0fd7ee969d2]] [formerly 091ce126d387faf09b6155c3307975ed2d633140] [formerly 88a5461284d1aa551986aeaf2328aa625587058f [formerly 91b1c2445886d7980c893df9e7073cb953601d3c] [formerly 88a5461284d1aa551986aeaf2328aa625587058f [formerly 91b1c2445886d7980c893df9e7073cb953601d3c] [formerly 91b1c2445886d7980c893df9e7073cb953601d3c [formerly 0082680f449d4f61de222776dd5877e46415a8f0]]]]
    Former-commit-id: 88a5461284d1aa551986aeaf2328aa625587058f [formerly 91b1c2445886d7980c893df9e7073cb953601d3c]
    Former-commit-id: 1d143d1c483c12b620edeb990f45511d154a368a
    Former-commit-id: 1603b3fd468afb86ba1d0848180a1619d6b8052e [formerly bec20f52f5f0e8e3765cc9b9b5fbbc539e703b9f]
    Former-commit-id: 0428d041d26cd53bf7b363e343ee54333224dd70

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
 if [ $1 ] ; then 
     if [ "$1" = itest ] ; then 
-        python -m unittest tests.integration_test.IntegrationTest
+        python -m unittest tests.integration_test.IntegrationTest.$2
     else
         python -m unittest tests.test.$1
     fi
