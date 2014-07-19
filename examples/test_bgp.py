6f848266edd0817eadd2cc1873aba33a4f8e86ec
Mark Watts <markw@cs.utexas.edu>
2014-07-18 19:25:35 -0500

    Updated tests
    Former-commit-id: 20137548d1760b8861ec1c1d8500769c6491eb30 [formerly b70fcb8f5d8f432f34c43a726596448dbf13116f] [formerly d0882cf1fd3fecf949abe8d32a323fc59f218f4d [formerly b336740e0c083b6f45ef9411b2f953c53b186751]] [formerly 2197431712ff84d059d5b8645f6782b6f46666f2] [formerly bfc5869fcce4aa6f1dbb6715adb875bae9d77515 [formerly bfc5869fcce4aa6f1dbb6715adb875bae9d77515 [formerly bfc5869fcce4aa6f1dbb6715adb875bae9d77515 [formerly 819601e58be0723983d61d90ee826701e7dec212]]]]
    Former-commit-id: bfc5869fcce4aa6f1dbb6715adb875bae9d77515
    Former-commit-id: 0134b159daea5037720f2bfdc893327e9a32c9c8
    Former-commit-id: 5d1ee8fe229eb912a550bb148c89b0d1ceeaa475 [formerly fde9529eff471fe22ebcb21167ed3197221fc276]
    Former-commit-id: 9314aaec729a82fc3b4edbf548d08ae9729b137c

--- examples/test_bgp.py
+++ examples/test_bgp.py
@@ -3,16 +3,16 @@ sys.path.insert(0,'..')
 
 import PyOpenWorm as P
 
-P.connect(configFile="../tests/test.conf", do_logging=False)
+P.connect(configFile="readme.conf", do_logging=False)
 try:
     #tests = [P.Neuron(),P.Neuron(lineageName="AB"),P.Neuron(name="ADAL"),P.Connection(),P.Connection(pre_cell="ADAL",post_cell="PVCR",syntype='send',synclass='Acetycholine')]
     #tests[len(tests)-1].save()
     #for x in tests[len(tests)-2].load():
         #print x
-    P.Connection(pre_cell='PVCL', post_cell='AVAL').save()
-    P.Connection(pre_cell='AVAL', post_cell='PVCR', number=30).save()
-    P.Connection(pre_cell='AVAL', post_cell='PVCR', number=10).save()
-    P.Connection(pre_cell='AVAL', post_cell='LOLR', number=30).save()
+    #P.Connection(pre_cell='PVCL', post_cell='AVAL').save()
+    #P.Connection(pre_cell='AVAL', post_cell='PVCR', number=30).save()
+    #P.Connection(pre_cell='AVAL', post_cell='PVCR', number=10).save()
+    #P.Connection(pre_cell='AVAL', post_cell='LOLR', number=30).save()
     query_object = P.Connection(pre_cell='AVAL')
     print 'STARTING WITH AVAL'
     for x in query_object.load():
@@ -36,7 +36,21 @@ try:
     print
     print 'NEIGHBORS of AVAL with number=30 connections'
     query_object = P.Neuron(name='AVAL')
-    for x in query_object.neighbor():
+    for x in query_object.neighbor(number=30):
         print next(x.name())
+    print
+    print 'NEURONS and their RECEPTORS'
+    query_object = P.Neuron()
+    for x in query_object.load():
+        # Wrap in a try-block in case there are no receptors listed
+        try:
+            print next(x.receptor())
+        except:
+            pass
+    print
+    print 'EVERYTHING'
+    for x in P.DataObject().load():
+        for y in x.load():
+            print y
 finally:
     P.Configureable.default.closeDatabase()
