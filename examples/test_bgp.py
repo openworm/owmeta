749a9d6e27a7753f78384f1abfd93d2b1429ec3f
Mark Watts <markw@cs.utexas.edu>
2014-07-20 23:42:09 -0500

    Turnning off logging in test.py
    Former-commit-id: 2d94db44680358f9f99e51fdd1d4ac2e66f3303b

--- examples/test_bgp.py
+++ examples/test_bgp.py
@@ -5,14 +5,6 @@ import PyOpenWorm as P
 
 P.connect(configFile="readme.conf", do_logging=False)
 try:
-    #tests = [P.Neuron(),P.Neuron(lineageName="AB"),P.Neuron(name="ADAL"),P.Connection(),P.Connection(pre_cell="ADAL",post_cell="PVCR",syntype='send',synclass='Acetycholine')]
-    #tests[len(tests)-1].save()
-    #for x in tests[len(tests)-2].load():
-        #print x
-    #P.Connection(pre_cell='PVCL', post_cell='AVAL').save()
-    #P.Connection(pre_cell='AVAL', post_cell='PVCR', number=30).save()
-    #P.Connection(pre_cell='AVAL', post_cell='PVCR', number=10).save()
-    #P.Connection(pre_cell='AVAL', post_cell='LOLR', number=30).save()
 
     query_object = P.Connection(pre_cell='AVAL')
     #sys.exit()
