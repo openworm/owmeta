350b9e0daf99a473d2a4bf6083d025f2fe1d233d
Mark Watts <markw@cs.utexas.edu>
2014-07-22 12:35:38 -0500

    More updates to documentation
    Former-commit-id: 0aba75a3ed464da6c44b22d03a1ca39ee5a48abb [formerly 74b9e987b00ef2662acc1cb01197216f1ee3a725]
    Former-commit-id: 68b615439127f6f01143a6d2bda3dfdc368c476a

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 numpydoc_show_class_members = False
+autodoc_default_flags = ['members','show-inheritance']
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo', 'sphinx.ext.autosummary', 'sphinx.ext.autodoc', 'numpydoc']
