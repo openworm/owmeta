96769356d56b79fb687ea08e110757304eb12821
Mark Watts <markw@cs.utexas.edu>
2014-07-25 12:41:27 -0500

    More documentation
    - slight changes to the 'values' class
    
    
    Former-commit-id: 5b7d2e30711b972d862757a3bc096d6c1b3628c7 [formerly 0d2da54486a08019b71e4803604cd9edf2788eb2] [formerly 02d2db4fe8352a33bbec52b2ddff3a561fae628c] [formerly 4d0cda132951d35407044cb14c76407724f715e0 [formerly 4d0cda132951d35407044cb14c76407724f715e0 [formerly 35e1faf979147a50025310637e8c7001cb48d46a]]]
    Former-commit-id: 4d0cda132951d35407044cb14c76407724f715e0
    Former-commit-id: 8ee4aad9ac76cfdea1dd5286bd3295aca7475a58
    Former-commit-id: 4e46e0c7a005808767e1db24fbe23d930f72ceb4

--- PyOpenWorm/worm.py
+++ PyOpenWorm/worm.py
@@ -21,10 +21,16 @@ class Worm(DataObject):
 
     """
 
-    def __init__(self,**kwargs):
+    def __init__(self,scientific_name=False,**kwargs):
         DataObject.__init__(self,**kwargs)
+        P.DatatypeProperty("scientific_name", owner=self)
         P.ObjectProperty("neuron_network", owner=self, value_type=P.Network)
         P.ObjectProperty("muscle", owner=self, value_type=P.Muscle)
+
+        if scientific_name:
+            self.scientific_name(scientific_name)
+        else:
+            self.scientific_name("C. elegans")
 
     def get_neuron_network(self):
         """
