7ee9926390e2e1873b3798ca91077ec88fd59c3f
Mark Watts <markw@cs.utexas.edu>
2014-08-07 20:11:25 -0500

    Removing my server from example/update scripts
    Former-commit-id: 10b7cf8765c7e0a89f6f37192a4c1a209a19ff85

--- examples/morpho.py
+++ examples/morpho.py
@@ -1,7 +1,6 @@
 import PyOpenWorm as P
-d = P.Data.open('../tests/test.conf')
-d['rdf.store_conf'] = ("http://107.170.133.175:8080/openrdf-sesame/repositories/OpenWorm2","http://107.170.133.175:8080/openrdf-sesame/repositories/OpenWorm2/statements")
-P.connect(conf=d)
+P.connect()
 aval = P.Cell(name="AVAL")
 morph = aval.morphology()
 print morph
+P.disconnect()
