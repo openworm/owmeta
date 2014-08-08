c608401eb6c7ef96a24d00efa195e8a5a05c436f
Mark Watts <markw@cs.utexas.edu>
2014-08-08 14:41:23 -0500

    Adding NMJs from sqldb in insert_worm.py
    - restricting worm identifier generation by worm name
    
    
    Former-commit-id: 0ab456347f0a63744237f63d1e212da676f54c2d [formerly 921e973187d2d2260255699dc4e44a06176a8e6f]
    Former-commit-id: 55b6e1a14766fd993f7175c31c8a5c2fd769b398

--- PyOpenWorm/worm.py
+++ PyOpenWorm/worm.py
@@ -12,6 +12,10 @@ from PyOpenWorm import DataObject
 
 class Worm(DataObject):
     """
+    A worm
+
+    All worms with the same name are considered to be the same object.
+
     Attributes
     ----------
     neuron_network : ObjectProperty
@@ -23,7 +27,7 @@ class Worm(DataObject):
 
     def __init__(self,scientific_name=False,**kwargs):
         DataObject.__init__(self,**kwargs)
-        Worm.DatatypeProperty("scientific_name", owner=self)
+        self.name = Worm.DatatypeProperty("scientific_name", owner=self)
         Worm.ObjectProperty("neuron_network", owner=self, value_type=P.Network)
         Worm.ObjectProperty("muscle", owner=self, value_type=P.Muscle)
         Worm.ObjectProperty("cell", owner=self, value_type=P.Cell)
@@ -62,3 +66,31 @@ class Worm(DataObject):
          """
 
         return self['semantic_net']
+
+    def identifier(self, *args, **kwargs):
+        # Shamelessly copy-pasted from cell.py
+
+        # If the DataObject identifier isn't variable, then self is a specific
+        # object and this identifier should be returned. Otherwise, if our name
+        # attribute is _already_ set, then we can get the identifier from it and
+        # return that. Otherwise, there's no telling from here what our identifier
+        # should be, so the variable identifier (from DataObject.identifier() must
+        # be returned
+        ident = DataObject.identifier(self, *args, **kwargs)
+        if 'query' in kwargs and kwargs['query'] == True:
+            if not DataObject._is_variable(ident):
+                return ident
+
+            if len(self.name.v) > 0:
+                # name is already set, so we can make an identifier from it
+                n = next(self.name())
+                return self.make_identifier(n)
+            else:
+                return ident
+        else:
+            if len(self.name.v) > 0:
+                # name is already set, so we can make an identifier from it
+                n = next(self.name())
+                return self.make_identifier(n)
+            else:
+                return ident
