fd092ca42afe6623e5ba80e2038f048140a46ab2
Mark Watts <markw@cs.utexas.edu>
2014-08-11 12:24:44 -0500

    Making insert_worm.py run on install
    - Untested on PyPi
    
    
    Former-commit-id: 97c38ab5d572d110e8b930ba4517a9d371e17081 [formerly b8721bb5752cf4deda1b47fd238d7071bac32fac] [formerly d81968e7d9cc40ec79c4b7ebeb55cfe625f1eadc [formerly 1e46333f6672b7d5fe8551452b69cdd692716f31]] [formerly 6af9fece7e27582e56f3277605e2b12e6c0e575d] [formerly 8b27c2cb0759fa728e6857a5f25c6e475fd12fe9 [formerly 8b27c2cb0759fa728e6857a5f25c6e475fd12fe9 [formerly 8b27c2cb0759fa728e6857a5f25c6e475fd12fe9 [formerly e03a9df6eaaaf5d5ec9012ff3c5651c669fd6f27]]]]
    Former-commit-id: 8b27c2cb0759fa728e6857a5f25c6e475fd12fe9
    Former-commit-id: d4a07f063eabc81f1dcd0df7fb38448d9cf9f701
    Former-commit-id: 5d4072d6b1a294928525b53df7bca87c7f526d5c [formerly 97e19913411f2fbe14d00211f01ca35ba439b493]
    Former-commit-id: 8965937a715870d0e9e5d0caa5d2a21b0a5ed575

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import sys
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+import os
+# Build the database if we're doing an install
+if (len(sys.argv) > 1) \
+    and (sys.argv[1] in ('install','build')) \
+    and not os.path.isdir('./db/worm.db'):
+    from db import insert_worm
+    # From: http://stackoverflow.com/questions/431684/how-do-i-cd-in-python
+    #------------
+    class cd:
+        """Context manager for changing the current working directory"""
+        def __init__(self, newPath):
+            self.newPath = newPath
+
+        def __enter__(self):
+            self.savedPath = os.getcwd()
+            os.chdir(self.newPath)
+
+        def __exit__(self, etype, value, traceback):
+            os.chdir(self.savedPath)
+    #-----------
+
+    with cd('./db'):
+        print 'Compiling C. elegans data'
+        insert_worm.do_insert()
+
+    if not os.path.isdir('./db/worm.db'):
+        sys.exit(-1)
 
 long_description = open("README.md").read()
 
@@ -11,7 +40,10 @@ setup(
     install_requires=required,
     name = "PyOpenWorm",
     version = '0.5.0-alpha',
-    packages = ['PyOpenWorm', 'tests'],
+    packages = ['PyOpenWorm', 'tests', 'db'],
+    package_data = dict(
+        db=['default.conf', 'worm.db/*']
+        ),
     author = "OpenWorm.org authors and contributors",
     author_email = "info@openworm.org",
     description = "A Python library for working with OpenWorm data and models",
