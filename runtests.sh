171706f95825b606979326618ed4c0d1166a8bdf
Mark Watts <markw@cs.utexas.edu>
2014-08-15 17:15:09 -0500

    Switching travis to using runtests.sh
    Former-commit-id: f70a9e5a94665d43bb333204e6500ee3ac98e0b6 [formerly 654cb126ab280f22abd15f174bbeae2f7cce4b66] [formerly b63718c8651d55bd2fa2dd15ee07b90ef48efe2a] [formerly 860886a251318ebdd0789b50908402c8f7ded299 [formerly 860886a251318ebdd0789b50908402c8f7ded299 [formerly 902d0a5ac1065247261b4542240b786af070b25a]]]
    Former-commit-id: 860886a251318ebdd0789b50908402c8f7ded299
    Former-commit-id: 68a96aad6cdcabc3257f64c2bc2c066bde2aa527
    Former-commit-id: 71d115d11b6dfcf644f5f99e11bd84a69789a2e1

--- runtests.sh
+++ runtests.sh
@@ -10,13 +10,19 @@ if [ $1 ] ; then
         python -m unittest tests.test.$1
     fi
 else
+    test_result=0
     for x in tests/test_*.conf ; do
         cp $x tests/_test.conf
         echo Testing with $x
         python -m unittest tests.test 2> ${x/.conf}.log
+        if [ $test_result -ne 0 ] ; then
+            test_result=1
+        fi
         tail -n 3 ${x/.conf}.log
         echo -----------------------------------------------------------------
     done
+    echo $test_result
+    exit $test_result
     #python -m unittest tests.integration_test
 fi
 
