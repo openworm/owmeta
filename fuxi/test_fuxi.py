1e79f020652477583be1feda763853fc8c9d5003
Travis Jacobs <trav221@gmail.com>
2014-08-21 16:01:39 -0230

    Changes demo files; revised readme.md
    Former-commit-id: a069b1b325cc245fdf174c8967b5d16d9d30bcad [formerly 2ce70e186aef298f0daa220759972edb0c5d6d21] [formerly d3238f1a2301e0778e6373d0e03c10edacd06ca1 [formerly fab15320fccf6edd1d9e669a7609bc0a189b2c42]]
    Former-commit-id: d3238f1a2301e0778e6373d0e03c10edacd06ca1
    Former-commit-id: 9b02aaca297f48ff81e9060cdf08a6c9e4669931

--- fuxi/test_fuxi.py
+++ fuxi/test_fuxi.py
@@ -5,9 +5,21 @@ from FuXi.Rete.Util import generateTokenSet
 from FuXi.Horn.HornRules import HornFromN3
 
 rule_store, rule_graph, network = SetupRuleStore(makeNetwork=True)
+
 closureDeltaGraph=Graph()
+
 network.inferredFacts = closureDeltaGraph
+
 for rule in HornFromN3('demo/rules.n3'): network.buildNetworkFromClause(rule)
+
 factGraph = Graph().parse('demo/facts.n3',format='n3')
+
 network.feedFactsToAdd(generateTokenSet(factGraph))
-print closureDeltaGraph.serialize(format='n3')
+
+inferred_facts = closureDeltaGraph.serialize(format='n3')
+
+inferred = open('inferred.n3', 'w')
+
+inferred.write(inferred_facts)
+
+inferred.close()
