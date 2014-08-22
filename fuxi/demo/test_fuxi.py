c0c80c70b79e7b6c308d93f690551694489ca395
Travis Jacobs <trav221@gmail.com>
2014-08-22 16:34:18 -0230

    Modifies insert_worm.py to use FuXi
    Readme.md updated, demo simplified
    
    
    Former-commit-id: cd439349cf07fd788607122a28cd8014f0a72b2c [formerly cd439349cf07fd788607122a28cd8014f0a72b2c [formerly 3ebe37fc013887919aea814ccc6788d3ab840423]]
    Former-commit-id: 83ccc876e678860fa70bb94c339856589c290077
    Former-commit-id: 902ea90c9c2427ec18717740f88f514af1222b08

--- fuxi/demo/test_fuxi.py
+++ fuxi/demo/test_fuxi.py
@@ -6,20 +6,20 @@ from FuXi.Horn.HornRules import HornFromN3
 
 rule_store, rule_graph, network = SetupRuleStore(makeNetwork=True)
 
-closureDeltaGraph=Graph()
+closureDeltaGraph = Graph()
 
 network.inferredFacts = closureDeltaGraph
 
-for rule in HornFromN3('demo/rules.n3'): network.buildNetworkFromClause(rule)
+for rule in HornFromN3('rules.n3'): 
+    network.buildNetworkFromClause(rule)
 
-factGraph = Graph().parse('demo/facts.n3',format='n3')
+factGraph = Graph().parse('facts.n3',format='n3')
 
 network.feedFactsToAdd(generateTokenSet(factGraph))
 
 inferred_facts = closureDeltaGraph.serialize(format='n3')
 
+#write inferred facts to file
 inferred = open('inferred.n3', 'w')
-
 inferred.write(inferred_facts)
-
 inferred.close()
