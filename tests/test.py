d916ba9ae9912508228bb3e7c53dcfe4cb5a1acc
Mark Watts <markw@cs.utexas.edu>
2014-08-26 13:55:16 -0500

    Adding a test for `Property.one`
    Former-commit-id: 2b2e9465f58b5a26238aed59bb141234f29e82db

--- tests/test.py
+++ tests/test.py
@@ -956,7 +956,20 @@ class DataTest(unittest.TestCase):
             disconnect()
 
 class PropertyTest(_DataTest):
-    pass
+    def test_one(self):
+        """ `one` should return None if there isn't a value or just the value if there is one """
+        class T(Property):
+            def __init__(self):
+                Property.__init__(self)
+                self.b = False
+
+            def get(self):
+                if self.b:
+                    yield "12"
+        t = T()
+        self.assertIsNone(t.one())
+        t.b=True
+        self.assertEqual('12', t.one())
 
 class SimplePropertyTest(_DataTest):
     def __init__(self,*args,**kwargs):
