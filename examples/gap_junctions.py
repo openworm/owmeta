213a14a44f37bd6fb42098913dde1d7412100754
Travis Jacobs <trav221@gmail.com>
2014-12-09 13:39:24 -0330

    Went through apsp.py, aval.xml, default.conf, and gap_junctions.py
    Former-commit-id: 943908195154e3bec753e6b980b572debce7bc66 [formerly 943908195154e3bec753e6b980b572debce7bc66 [formerly 8684ea1b4ad42f1e83793669c4f97f8fe711b6ee]]
    Former-commit-id: e3399c346994336756f07b72dadb35ca1733bc5e
    Former-commit-id: fa3b19507458bbe286586ba6e5c219ec84847d6f

--- examples/gap_junctions.py
+++ examples/gap_junctions.py
@@ -1,10 +1,28 @@
+"""
+Demo:
+"""
+
 import sys
 sys.path.insert(0,'..')
 import PyOpenWorm as P
-P.connect(testConfig=True)
+
+#Create dummy database configuration.
+d = P.Data({
+    "rdf.upload_block_statement_count" : 50,
+    "user.email" : "jerry@cn.com"
+})
+
+P.connect(conf=d)
 n = P.Neuron(name='AVAL')
 # insert some connections
-#n1 = Neuron("DA3")
-#c = Connection(pre=n,post=n1,synclass="send")
-for x in n.connection():
+n1 = P.Neuron("DA3")
+c = P.Connection(pre_cell=n, post_cell=n1, syntype="send")
+
+n.connection.set(c)
+
+print n.connection.get(pre_post_or_either='either')
+
+print n.conns
+
+for x in n.connection:
     print x
