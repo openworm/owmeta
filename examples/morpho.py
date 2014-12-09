9abc4df88796692014db08f973db7a44faaf7e39
Travis Jacobs <trav221@gmail.com>
2014-12-09 16:59:59 -0330

    Annotate morpho.py
    Former-commit-id: 44d0797e2683a75dc85776117d1ba7d97eb5f890

--- examples/morpho.py
+++ examples/morpho.py
@@ -1,6 +1,23 @@
+"""
+How to load morphologies of certain cells from the database.
+"""
 import PyOpenWorm as P
-P.connect()
+
+#Create dummy database configuration. Normally you would connect to the actual database.
+d = P.Data({
+    "rdf.upload_block_statement_count" : 50,
+    "user.email" : "jerry@cn.com"
+})
+
+#Connect to database with dummy configuration.
+P.connect(conf=d)
+
+#Create a new Cell object to work with.
 aval = P.Cell(name="AVAL")
+
+#Get the morphology associated with the Cell. Returns a neuroml.Morphology object.
 morph = aval.morphology()
 print morph
+
+#Disconnect from the database.
 P.disconnect()
