36cffcab9292d2281d251c3909a0b0898c51b2a3
Travis Jacobs <trav221@gmail.com>
2014-12-23 18:01:37 -0330

    Finish cleaning up/testing the examples.
    Former-commit-id: 98e72753e89e2b00ef969b46e373edbb2820fd44 [formerly beed36651e1ef186fff249ddbd7aec32f7b40783] [formerly d956a678e542069f79aebd1acfaf6010580867f6 [formerly 716dd2cf5154317f95151eb5ef95febb422ae21f]] [formerly 5fa2635de4698c5667da1504d0b1fba29827c1ac] [formerly 8cb66fb9a9a84968a0dda8b45011ee408e4b2c09 [formerly 8cb66fb9a9a84968a0dda8b45011ee408e4b2c09 [formerly 8cb66fb9a9a84968a0dda8b45011ee408e4b2c09 [formerly 72a520c7ab0e4c0c822c3419332628bc65c313cf]]]]
    Former-commit-id: 8cb66fb9a9a84968a0dda8b45011ee408e4b2c09
    Former-commit-id: b71d9e51cc248ef8ce519e8d5a6edae5af3db468
    Former-commit-id: fd8cada22d9f527eea0acd4fd3176e6e11eb2d67 [formerly ead52025ffe39c6db2baa6d9817ab487b1eef805]
    Former-commit-id: a3fb65cd445ab678da7358672e91909d1111526e

--- examples/NeuronBasicInfo.py
+++ examples/NeuronBasicInfo.py
@@ -1,20 +1,44 @@
+"""
+Get information about a neuron from the database.
+In this case we are getting a list of neurons associated with each neuron type.
+
+Note how the attributes of a neuron are accessed: `list(neuron.attribute())`
+where attribute is one of the attributes outlined in the neuron API.
+
+Also note that, for attributes with only one value, the value can be accessed by
+doing `neuron.attribute.one()`.
+
+Example: Something like `adal.name.one()` might return "ADAL", assuming we have
+declared the variable `adal`.
+"""
+
 import PyOpenWorm as P
 
+#Connect to the database.
 P.connect("default.conf")
 
+#Get the worm object.
 worm = P.Worm()
 
+#Extract the network object from the worm.
 net = worm.get_neuron_network()
 
+#Declare an empty dictionary.
 types = {}
+
+#Go through the neuron objects in the network.
 for neuron in net.neuron():
     if len(list(neuron.type())) > 0:
+        #i.e. "If there is a type associated with a neuron:"
         type = list(neuron.type())[0]
 
         if not types.has_key(type):
+            #If this type key is not already in our dictionary, add it.
             types[type] = []
 
+        #Add the neuron's name under its type key in our dictionary.
         types[type].append(list(neuron.name())[0])
 
 for type in types.keys():
+    #Print out the types of neurons and list the associated neurons with each.
     print("Neurons of type %s:\n  %s\n"%(type, types[type]))
