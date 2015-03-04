9cc7b91713719b42addc54ef52a6b887c364248a
travs <trav221@gmail.com>
2015-03-03 21:25:04 -0330

    Adding condition property as classmethod of Experiment
    Former-commit-id: 2791b322050751176cd17f243ce918735eef502e [formerly 87bdf12df49179ad44f0fe52c2b4665e61b700a5] [formerly 6d59c7de23cc3aab9665c47e95478dff9e4ad3d3] [formerly 4f69480e12e31c78480b22d08b760483b8661db0]
    Former-commit-id: c60655f98f8ccaf74fda0614827b6f511590b173
    Former-commit-id: 4614b0ab163669138b5176aba8f2bea492d55339
    Former-commit-id: 11fd71e337b98a391cbeec672a408380bce37dfd

--- PyOpenWorm/experiment.py
+++ PyOpenWorm/experiment.py
@@ -1,19 +1,5 @@
 from PyOpenWorm import *
 
-class Conditions(Property):
-    def __init__(self, **kwargs):
-        Property.__init__(self, 'conditions', **kwargs)
-        self._conds = {}
-
-    def get(self, **kwargs):
-        """
-        Get the conditions of an experiment object.
-
-        Returns
-        -------
-        Python dict of experimental conditions.
-        """
-
 class Experiment(DataObject):
     """
     Generic class for storing information about experiments
@@ -32,8 +18,12 @@ class Experiment(DataObject):
     def __init__(self, ref, **kwargs):
         DataObject.__init__(self, **kwargs)
         Experiment.ObjectProperty("reference", self, value_type=Experiment)
-        # Get conditions associated with this experiment
-        Conditions(owner=self)
 
         if(isinstance(ref,Experiment)):
             self.reference(ref)
+
+    @classmethod
+    def Condition(cls, cond, val):
+        cls.DatatypeProperty.__init__(cond, self, **kwargs)
+        self.cond(val)
+
