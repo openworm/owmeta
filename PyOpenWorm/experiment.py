769772019862bcc5b07cd06f304fca8ffed07d9f
travs <trav221@gmail.com>
2015-03-09 14:05:11 -0230

    Add docstrings for condition.
    Former-commit-id: 29c01abf8ea63dcbbb779896b18988411c86f2dd [formerly 51e9a078397e5833065ca7d898f03a5f249deba9] [formerly 6276203f0a9c8628c8490e5ff98d01be03b177f3 [formerly 3426439cef2ee49e0c2e5e685b54d77a6df57472]] [formerly 1165d80accd3535d40c21a39f6b6efba47864b3c] [formerly ee96f097b058ac1b5d0297ba412e3538aa34516f [formerly 9f6cc09b3601786fe4d62cf3414f8e8dcf4b9ae9]]
    Former-commit-id: ee96f097b058ac1b5d0297ba412e3538aa34516f
    Former-commit-id: 1ec70b82595e3ed307e0c49da69d0f59abfdb687
    Former-commit-id: 7d73e0ee8698de2e1f09202257dfdfc235927189 [formerly b65b9c6354611a5956fab4bd4340b67c23cf2b58]
    Former-commit-id: 6997628e56cdfeb16e8af5f5e48871ca64ec88a6

--- PyOpenWorm/experiment.py
+++ PyOpenWorm/experiment.py
@@ -1,5 +1,46 @@
 from PyOpenWorm import *
 
+class Condition(Property):
+    """
+    Class for storing a condition of an experiment.
+    Takes one value and stores it with a key.
+
+    Parameters
+    ----------
+    condition : String
+        Name of the condition (ex: "subject").
+    value : any
+        State of the condition for the experiment in question.
+        Could be an object, string, integer; whatever is relevant.
+
+    Attributes
+    ----------
+    condition : String
+        Name of the condition (ex: "subject").
+    value : any
+        State of the condition for the experiment in question.
+        Could be an object, string, integer; whatever is relevant.
+    """
+    multiple=True
+    def __init__(self, *args, **kwargs):
+        Property.__init__(self, 'condition', *args, **kwargs)
+        self._conds = {}
+
+    def get(self):
+        #get conditions and values from the db
+        if len(self._conds) > 0:
+            for c in self._conds:
+                yield c
+        else:
+            pass
+
+    def set(self, condition, value):
+        #add condition as datatype property to the experiment object
+        Experiment.DatatypeProperty(condition, self)
+        exec("self." + condition + "(value)" )
+        #now add it to the _conds dictionary
+        self._conds[condition] = value
+
 class Experiment(DataObject):
     """
     Generic class for storing information about experiments
@@ -11,7 +52,7 @@ class Experiment(DataObject):
 
     Attributes
     ----------
-    conditions : Property
+    condition : Property
         Experimental conditions, set by key.
     """
 
@@ -22,6 +63,4 @@ class Experiment(DataObject):
         if(isinstance(ref,Evidence)):
             self.reference(ref)
 
-    def Condition(self, cond, val):
-        Experiment.DatatypeProperty(cond, self)
-        exec("self." + cond + "(val)" )
+        Condition(owner=self)
