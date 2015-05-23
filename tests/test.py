fb16df875bb4f745561093fd765d7f528440324a
Gopal Sarma <gsarma.web@gmail.com>
2015-05-23 15:09:54 -0400

    Pair programming with @travs.  More factoring out of tests: 1) ConfigureTest 2) ConfigureableTest 3) DataObjectTestToo
    Former-commit-id: 015c477505e2c15f031d226e9b541f4ef1930021 [formerly df2742bafc4e32706fab3a3b080149ce3dc599f4] [formerly 4349e70fe4cb07bedaa31f1fef2c249e97941f0f] [formerly c4e1bba9b0e02555894840aca37122bd2a141bc7]
    Former-commit-id: cc581257d85f36fe26645c43c60ab47c916772fd
    Former-commit-id: 57c6e83a56e134a8d37de273f6a26c52ac6f32a0
    Former-commit-id: 8bee17742da7e1d3c6aaece70a5bb025cac2324b

--- tests/test.py
+++ tests/test.py
@@ -62,81 +62,14 @@ from ExampleRunnerTest import ExampleRunnerTest
 # Need description of these tests
 from DataIntegrityTest import DataIntegrityTest
 
-class ConfigureTest(unittest.TestCase):
-    def test_fake_config(self):
-        """ Try to retrieve a config value that hasn't been set """
-        with self.assertRaises(KeyError):
-            c = Configure()
-            c['not_a_valid_config']
+# Need description of these tests
+from ConfigureTest import ConfigureTest
 
-    def test_literal(self):
-        """ Assign a literal rather than a ConfigValue"""
-        c = Configure()
-        c['seven'] = "coke"
-        self.assertEqual(c['seven'], "coke")
+# Need description of these tests
+from ConfigureableTest import ConfigureableTest
 
-    def test_ConfigValue(self):
-        """ Assign a ConfigValue"""
-        c = Configure()
-        class pipe(ConfigValue):
-            def get(self):
-                return "sign"
-        c['seven'] = pipe()
-        self.assertEqual("sign",c['seven'])
-
-    def test_getter_no_ConfigValue(self):
-        """ Assign a method with a "get". Should return a the object rather than calling its get method """
-        c = Configure()
-        class pipe:
-            def get(self):
-                return "sign"
-        c['seven'] = pipe()
-        self.assertIsInstance(c['seven'], pipe)
-
-    def test_late_get(self):
-        """ "get" shouldn't be called until the value is *dereferenced* """
-        c = Configure()
-        a = {'t' : False}
-        class pipe(ConfigValue):
-            def get(self):
-                a['t'] = True
-                return "sign"
-        c['seven'] = pipe()
-        self.assertFalse(a['t'])
-        self.assertEqual(c['seven'], "sign")
-        self.assertTrue(a['t'])
-
-    def test_read_from_file(self):
-        """ Read configuration from a JSON file """
-        try:
-            d = Data.open("tests/test.conf")
-            self.assertEqual("test_value", d["test_variable"])
-        except:
-            self.fail("test.conf should exist and be valid JSON")
-
-    def test_read_from_file_fail(self):
-        """ Fail on attempt to read configuration from a non-JSON file """
-        with self.assertRaises(ValueError):
-            Data.open("tests/bad_test.conf")
-
-
-class ConfigureableTest(unittest.TestCase):
-    def test_init_empty(self):
-        """Ensure Configureable gets init'd with the defalut if nothing's given"""
-        i = Configureable()
-        self.assertEqual(Configureable.conf,i.conf)
-
-    def test_init_False(self):
-        """Ensure Configureable gets init'd with the defalut if False is given"""
-        i = Configureable(conf=False)
-        self.assertEqual(Configureable.conf, i.conf)
-
-class DataObjectTestToo(unittest.TestCase):
-    def test_helpful_message_on_non_connection(self):
-        """ The message should say something about connecting """
-        Configureable.conf = False # Ensure that we are disconnected
-        with self.assertRaisesRegexp(Exception, ".*[cC]onnect.*"):
-            do = DataObject()
+# Need description of these tests
+from DataObjectTestToo import DataObjectTestToo
 
 class _DataTest(unittest.TestCase):
     def delete_dir(self):
