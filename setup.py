1f94f520fc4c9bec336dd42cd17755b9064e94fe
Travis Jacobs <trav221@gmail.com>
2015-06-03 11:17:15 -0230

    Move requirements to setup.py
    Former-commit-id: ecf2ba5c235864cd262581133d9507a4809ab57b
    Former-commit-id: 8045e72806128a6ae49b3cb7d6b0353deb1a9b92

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def _post_install():
 
 class install(_install):
     def run(self):
-        _install.run(self)
+        self.do_egg_install()
         self.execute(_post_install, (), msg='Running post-install script(s)')
 
 with open('requirements.txt') as f:
@@ -29,7 +29,31 @@ long_description = open('README.md').read()
 setup(
     name = 'PyOpenWorm',
     cmdclass = {'install': install},
-    install_requires = required,
+    install_requires=[
+        'xlrd',
+        'libneuroml',
+        'zodb==4.1.0',
+        'Pint',
+        'rdflib>=4.1.2',
+        'BTrees==4.0.8',
+        'SPARQLWrapper==1.6.2',
+        'ZConfig==3.0.4',
+        'decorator==3.4.0',
+        'discover==0.4.0',
+        'html5lib==0.999',
+        'isodate==0.5.0',
+        'networkx==1.9',
+        'numpydoc==0.5',
+        'persistent==4.0.8',
+        'pyparsing==2.0.2',
+        'rdflib_zodb==1.0',
+        'requirements==0.1',
+        'transaction==1.4.3',
+        'wsgiref==0.1.2',
+        'zc.lockfile==1.1.0',
+        'zdaemon==4.0.0',
+        'zope.interface==4.1.1'
+    ],
     dependency_links = [
         'git://github.com/NeuralEnsemble/libNeuroML.git#egg=libNeuroML',
         'git://github.com/zopefoundation/ZODB.git#egg=ZODB',
@@ -42,7 +66,7 @@ setup(
     author_email = 'info@openworm.org',
     description = 'A Python library for working with OpenWorm data and models',
     long_description = long_description,
-    license = 'BSD',
+    license = 'MIT',
     url='http://PyOpenWorm.readthedocs.org/en/latest/',
     download_url = 'https://github.com/openworm/PyOpenWorm/archive/master.zip',
     classifiers = [
