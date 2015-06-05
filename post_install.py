b5d0735a454e46e312fad471d9cf48a86d2cdb01
Travis Jacobs <trav221@gmail.com>
2015-06-05 13:03:38 -0230

    Run insert_worm.py on install as non-root
    Former-commit-id: e3628d87be302bfe3f72161e3f5d7f364dfe9ea2 [formerly 117385a6bbc09b41db37c5b7f4d63b02fa50bfed] [formerly 7186aba8886afd35e708a55aca844d3a77aab292 [formerly 16772f29d56133976fd1d137e6e7abd167b7e5de]] [formerly 46a8588865a9fe15e9ba054a7c0ac43cb2e799bc] [formerly 05b27ff04ef98223e0f4167f7dff798af4f3d160 [formerly 0aec34f149e35dcf1f4ff874dad2e6009b1d24f0]]
    Former-commit-id: 05b27ff04ef98223e0f4167f7dff798af4f3d160
    Former-commit-id: 1c9b2bba9ad83f5bc2b716bcaab854c7b36808e0
    Former-commit-id: 1672669c4cabb6195bc1523cae5061d6f7735fe4 [formerly 3cb1c18cfecc0b06f2a020a2e6ff1665b06f703f]
    Former-commit-id: e7b40648c5c1b17b8d9a8529b7430334101dfea4

--- post_install.py
+++ post_install.py
@@ -1,17 +1,17 @@
+import sys
+del sys.path[0] # We want to get the installed PyOpenWorm, not the local one
 from setuptools import setup
 from subprocess import call
-import os, sys, shutil, glob
-
-# remove current dir from path for this script, since we want to find
-# newly installed PyOpenWorm module
-del sys.path[0]
-
-import PyOpenWorm
+import os, shutil, glob, PyOpenWorm
 
 package_location = PyOpenWorm.__path__[0]
 pwd = os.path.dirname(os.path.realpath(__file__))
+user_id = os.stat(pwd).st_uid # this is the person that cloned the repo
 script_location = os.path.join(pwd, 'OpenWormData', 'scripts')
-call([sys.executable, 'insert_worm.py'], cwd = script_location)
+user_script = 'insert_worm.py' # script(s) we want to be run as non-root
+
+print('Running {}'.format(user_script))
+call([sys.executable, user_script], cwd = script_location, preexec_fn=os.setuid(user_id))
 # move created database files to your library's package directory
 db_files = glob.glob(os.path.join(script_location, 'worm.db*'))
 for db_file in db_files:
@@ -20,4 +20,4 @@ for db_file in db_files:
   shutil.copy(db_file, package_location)
   os.chmod(new_location, 0777)
 # change directory owner to allow writing and reading from db in that dir
-os.chown(package_location, os.stat(pwd).st_uid, -1)
+os.chown(package_location, user_id, -1)
