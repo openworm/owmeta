5242d2b7cc68a74bef7736f7e5c5cfec21fa3026
Travis Jacobs <trav221@gmail.com>
2015-06-13 21:37:41 -0230

    Fix error where only connections to/from AVAL were being added
    This must have slipped in when experimenting with new ways to upload
    synapse data.
    
    
    Former-commit-id: 7b70b42d2ac4369817cbb9a0cff8884c5ff19d87 [formerly 9a5e5b1fd0de93111ed94ad2f79c5a2f8b3fd389] [formerly 4f281a9a438c907b63658574ee84e3452cf14e8b] [formerly cb44e9b976d60f0209ec36de3b013be336c80004]
    Former-commit-id: abd6cd68fb920402a55e8464f2d2fe432c55ecb6
    Former-commit-id: 9a32479075a90e1da9e908b9f937605b46c3f36c
    Former-commit-id: a63d4590a55918cbb4b60e02fe61551c332cb5eb

--- OpenWormData/scripts/insert_worm.py
+++ OpenWormData/scripts/insert_worm.py
@@ -226,7 +226,7 @@ def upload_synapses():
         # Get synapses and gap junctions and add them to the graph
         s = xlrd.open_workbook('../aux_data/NeuronConnect.xls').sheets()[0]
         for row in range(1, s.nrows):
-            if s.cell(row, 2).value in ('S', 'Sp', 'EJ') and 'AVAL' in [s.cell(row, 0).value, s.cell(row, 1).value]:
+            if s.cell(row, 2).value in ('S', 'Sp', 'EJ'):
                 #We're not going to include 'receives' ('R', 'Rp') since they're just the inverse of 'sends'
                 #Also omitting 'NMJ' for the time being (no model in db)
                 pre = s.cell(row, 0).value
@@ -239,7 +239,7 @@ def upload_synapses():
 
                 # Add them to a dict to make sure Sends ('S') and Send-polys ('Sp') are summed.
                 # keying by connection pairs as a string (e.g. 'SDQL,AVAL,send').
-                # values are lists if the form [pre, post, number, syntype].
+                # values are lists of the form [pre, post, number, syntype].
                 string_key = '{},{},{}'.format(pre, post, syntype)
                 if string_key in combining_dict.keys():
                     # if key already there, add to number
