896ed30837218b359b85d57fd53349a5f456fb71
Stephen Larson <stephen.larson@gmail.com>
2015-06-14 17:55:16 +0100

    moving documentation to cell, neuron, connection classes
    Former-commit-id: 51503de74c4a5b43e5a7eda95f1c2d811ccf3200 [formerly b0acf538422882b18818925bb3ddf0fcfff76440] [formerly ac92a2a0965c7a75c32789c6c26dd9b771eec40c] [formerly 8a1711cfadb8fb7f947b6252953c697a76fd0b2f]
    Former-commit-id: 60ce56ffd3e094b468dbe59014ae4bd51a7e5aee
    Former-commit-id: a3042e0e382990e27676fa9b9c69579de1c49f20
    Former-commit-id: e18478fd8a4356d8551f1adbed61e7a4190b5db6

--- PyOpenWorm/neuron.py
+++ PyOpenWorm/neuron.py
@@ -6,6 +6,107 @@ from PyOpenWorm import Cell
 
 # XXX: Should we specify somewhere whether we have NetworkX or something else?
 
+class Neuron(Cell):
+    """
+    A neuron.
+
+    Parameters
+    ----------
+    name : string
+        The name of the neuron.
+
+    Attributes
+    ----------
+    type : DatatypeProperty
+        The neuron type (i.e., sensory, interneuron, motor)
+    receptor : DatatypeProperty
+        The receptor types associated with this neuron
+    innexin : DatatypeProperty
+        Innexin types associated with this neuron
+    neurotransmitter : DatatypeProperty
+        Neurotransmitters associated with this neuron
+    neuropeptide : DatatypeProperty
+        Name of the gene corresponding to the neuropeptide produced by this neuron
+    neighbor : Property
+        Get neurons connected to this neuron if called with no arguments, or
+        with arguments, state that neuronName is a neighbor of this Neuron
+    connection : Property
+        Get a set of Connection objects describing chemical synapses or gap
+        junctions between this neuron and others
+
+    """
+    def __init__(self, name=False, **kwargs):
+        Cell.__init__(self,name=name,**kwargs)
+        # Get neurons connected to this neuron
+        Neighbor(owner=self)
+        # Get connections from this neuron
+        Connection(owner=self)
+
+        Neuron.DatatypeProperty("type",self, multiple=True)
+        Neuron.DatatypeProperty("receptor", self, multiple=True)
+        Neuron.DatatypeProperty("innexin", self, multiple=True)
+        Neuron.DatatypeProperty("neurotransmitter", self, multiple=True)
+        Neuron.DatatypeProperty("neuropeptide", self, multiple=True)
+        ### Aliases ###
+        self.get_neighbors = self.neighbor
+        self.receptors = self.receptor
+
+    def GJ_degree(self):
+        """Get the degree of this neuron for gap junction edges only
+
+        :returns: total number of incoming and outgoing gap junctions
+        :rtype: int
+        """
+        count = 0
+        for c in self.connection():
+            if c.syntype.one() == 'gapJunction':
+                count += 1
+        return count
+
+    def Syn_degree(self):
+        """Get the degree of a this neuron for chemical synapse edges only
+
+        :returns: total number of incoming and outgoing chemical synapses
+        :rtype: int
+        """
+        count = 0
+        for c in self.connection.get('either'):
+            if c.syntype.one() == 'send':
+                count += 1
+        return count
+
+    def _type_networkX(self):
+        """Get type of this neuron (motor, interneuron, sensory)
+
+        Use the networkX representation as the source
+
+        :returns: the type
+        :rtype: str
+        """
+        return self['nx'].node[self.name.one()]['ntype']
+
+
+    def get_incidents(self, type=0):
+        """ Get neurons which synapse at this neuron """
+        # Directed graph. Getting accessible _from_ this node
+        for item in self['nx'].in_edges_iter(self.name(),data=True):
+            if 'GapJunction' in item[2]['synapse']:
+                yield item[0]
+
+    def _as_neuroml(self):
+       """Return this neuron as a NeuroML representation
+
+          :rtype: libNeuroML.Neuron
+       """
+
+    def __str__(self):
+        n = self.name()
+        if n is not None:
+            return n
+        else:
+            return ""
+
+
 class Neighbor(P.Property):
     multiple=True
     def __init__(self,**kwargs):
@@ -42,6 +143,13 @@ class Neighbor(P.Property):
                 yield x
 
 class Connection(P.Property):
+    """A representation of the connection between neurons. Either a gap junction
+    or a chemical synapse
+
+    TODO: Add neurotransmitter type.
+    TODO: Add connection strength
+    """
+
     multiple=True
     def __init__(self,**kwargs):
         P.Property.__init__(self,'connection',**kwargs)
@@ -137,101 +245,3 @@ class Connection(P.Property):
         for c in self._conns:
             for x in c.triples(**kwargs):
                 yield x
-
-class Neuron(Cell):
-    """
-    A neuron.
-
-    Parameters
-    ----------
-    name : string
-        The name of the neuron.
-
-    Attributes
-    ----------
-    type : DatatypeProperty
-        The neuron type (i.e., sensory, interneuron, motor)
-    receptor : DatatypeProperty
-        The receptor types associated with this neuron
-    innexin : DatatypeProperty
-        Innexin types associated with this neuron
-    neurotransmitter : DatatypeProperty
-        Neurotransmitters associated with this neuron
-    neuropeptide : DatatypeProperty
-        Name of the gene corresponding to the neuropeptide produced by this neuron
-    neighbor : Property
-        Get neurons connected to this neuron
-    connection : Property
-        Get connections associated with this neuron
-    """
-    def __init__(self, name=False, **kwargs):
-        Cell.__init__(self,name=name,**kwargs)
-        # Get neurons connected to this neuron
-        Neighbor(owner=self)
-        # Get connections from this neuron
-        Connection(owner=self)
-
-        Neuron.DatatypeProperty("type",self, multiple=True)
-        Neuron.DatatypeProperty("receptor", self, multiple=True)
-        Neuron.DatatypeProperty("innexin", self, multiple=True)
-        Neuron.DatatypeProperty("neurotransmitter", self, multiple=True)
-        Neuron.DatatypeProperty("neuropeptide", self, multiple=True)
-        ### Aliases ###
-        self.get_neighbors = self.neighbor
-        self.receptors = self.receptor
-
-    def GJ_degree(self):
-        """Get the degree of this neuron for gap junction edges only
-
-        :returns: total number of incoming and outgoing gap junctions
-        :rtype: int
-        """
-        count = 0
-        for c in self.connection():
-            if c.syntype.one() == 'gapJunction':
-                count += 1
-        return count
-
-    def Syn_degree(self):
-        """Get the degree of a this neuron for chemical synapse edges only
-
-        :returns: total number of incoming and outgoing chemical synapses
-        :rtype: int
-        """
-        count = 0
-        for c in self.connection.get('either'):
-            if c.syntype.one() == 'send':
-                count += 1
-        return count
-
-    def _type_networkX(self):
-        """Get type of this neuron (motor, interneuron, sensory)
-
-        Use the networkX representation as the source
-
-        :returns: the type
-        :rtype: str
-        """
-        return self['nx'].node[self.name.one()]['ntype']
-
-
-    def get_incidents(self, type=0):
-        """ Get neurons which synapse at this neuron """
-        # Directed graph. Getting accessible _from_ this node
-        for item in self['nx'].in_edges_iter(self.name(),data=True):
-            if 'GapJunction' in item[2]['synapse']:
-                yield item[0]
-
-    def _as_neuroml(self):
-       """Return this neuron as a NeuroML representation
-
-          :rtype: libNeuroML.Neuron
-       """
-
-    def __str__(self):
-        n = self.name()
-        if n is not None:
-            return n
-        else:
-            return ""
-
