1a70209cd604897bea48dbd219816d69f0f7da57
Stephen Larson <stephen.larson@gmail.com>
2015-06-14 17:12:44 +0100

    incorporating worm and evidence rest into docstrings
    Former-commit-id: 1991854c7560b6b692a77b7b874548cead9aef83 [formerly b07834c0208c0fb8a3f42a133c54434368d1ed5d] [formerly 2ccabfd39933c1b2d8757e024abfb3cf063dff93 [formerly 8ae7e62b998aa370414099b2ae63a9d830e59471]] [formerly 24c985b2a50fce1d99732126e650932cc986a53b] [formerly a30e42e3dd4c140a0f1e63d4423ce7d2ff041ff4 [formerly c7f7e20d0885fa2049ff6cb40526e5cd44643a05]]
    Former-commit-id: a30e42e3dd4c140a0f1e63d4423ce7d2ff041ff4
    Former-commit-id: 6fa3e449e854a9a07d04cdaf1db723d043daccfc
    Former-commit-id: 8e3482271fc6807b55f500dafe8544869940daf8 [formerly a4d4668e3f31c48c87816a9e8faf968ca6b6859b]
    Former-commit-id: f4dc089aea7e574da3f05b8693c322727b2a1fed

--- PyOpenWorm/evidence.py
+++ PyOpenWorm/evidence.py
@@ -1,78 +1,6 @@
 from PyOpenWorm import *
 
-```
-Metadata in PyOpenWorm
-======================
 
-Attaching evidence
--------------------
-Attaching evidence to an object is as easy as::
-
-      e = Evidence(author='White et al.', date='1986')
-      e.asserts(Connection(pre_cell="VA11", post_cell="VD12"))
-      e.save()
-
-But what does this series of statements mean? For us it means that White et al.
-assert that "the cells VA11 and VD12 have a connection".
-In particular, it says nothing about the neurons themselves.
-
-Another example::
-
-      e = Evidence(author='Sulston et al.', date='1983')
-      e.asserts(Neuron(name="AVDL").lineageName("AB alaaapalr"))
-      e.save()
-
-This would say that Sulston et al. claimed that neuron AVDL has lineage AB alaaapalr.
-
-Now a more ambiguous example::
-
-      e = Evidence(author='Sulston et al.', date='1983')
-      e.asserts(Neuron(name="AVDL"))
-      e.save()
-
-What might this mean? There's no clear relationship being discussed as in the
-previous examples. There are two reasonable semantics for
-these statements. They could indicate that Sulston et al. assert everything
-about the AVDL (in this case, only its name). Or they could
-indicate that Sulston et al. state the existence of AVDL. We will assume the
-semantics of the latter for *most* objects. The second
-intention can be expressed as::
-
-      e = Evidence(author='Sulston et al.', date='1983')
-      e.asserts_all_about(Neuron(name="AVDL"))
-      e.save()
-
-`asserts_all_about` individually asserts each of the properties of the Neuron
-including its existence. It does not recursively assert
-properties of values set on the AVDL Neuron. If, for instance, the Neuron had a
-*complex object* as the value for its receptor types with
-information about the receptor's name primary agonist, etc., `asserts_all_about`
-  would say nothing about these. However, `asserts_all` (TODO)::
-
-      e.asserts_all(Neuron(name="AVDL",receptor=complex_receptor_object))
-
-would make the aforementioned recursive statement.
-
-Retrieving evidence
--------------------
-
-.. Not tested with the latest
-
-Retrieving evidence for an object is trivial as well ::
-
-      e = Evidence()
-      e.asserts(Connection(pre_cell="VA11", post_cell="VD12"))
-      for x in e.load():
-         print x
-
-This would print all of the evidence for the connection between VA11 and VD12
-
-It's important to note that the considerations of recursive evidence assertions
-above do not operate for retrieval. Only evidence for the
-particular object queried (the Connection in the example above), would be
-returned and not any evidence for anything otherwise about VA11
-or VD12.
-```
 
 class EvidenceError(Exception):
     pass
@@ -150,13 +78,122 @@ class AssertsAllAbout(Property):
 
 class Evidence(DataObject):
     """
-    A class for storing metadata, like scholarly references, for
-    other objects
+    A representation of some document which provides evidence like scholarly
+    references, for other objects.
+
+    Possible keys include::
+
+        pmid,pubmed: a pubmed id or url (e.g., 24098140)
+        wbid,wormbase: a wormbase id or url (e.g., WBPaper00044287)
+        doi: a Digitial Object id or url (e.g., s00454-010-9273-0)
+
+    Attaching evidence
+    -------------------
+    Attaching evidence to an object is as easy as::
+
+          e = Evidence(author='White et al.', date='1986')
+          e.asserts(Connection(pre_cell="VA11", post_cell="VD12"))
+          e.save()
+
+    But what does this series of statements mean? For us it means that White et al.
+    assert that "the cells VA11 and VD12 have a connection".
+    In particular, it says nothing about the neurons themselves.
+
+    Another example::
+
+          e = Evidence(author='Sulston et al.', date='1983')
+          e.asserts(Neuron(name="AVDL").lineageName("AB alaaapalr"))
+          e.save()
+
+    This would say that Sulston et al. claimed that neuron AVDL has lineage AB alaaapalr.
+
+    Now a more ambiguous example::
+
+          e = Evidence(author='Sulston et al.', date='1983')
+          e.asserts(Neuron(name="AVDL"))
+          e.save()
+
+    What might this mean? There's no clear relationship being discussed as in the
+    previous examples. There are two reasonable semantics for
+    these statements. They could indicate that Sulston et al. assert everything
+    about the AVDL (in this case, only its name). Or they could
+    indicate that Sulston et al. state the existence of AVDL. We will assume the
+    semantics of the latter for *most* objects. The second
+    intention can be expressed as::
+
+          e = Evidence(author='Sulston et al.', date='1983')
+          e.asserts_all_about(Neuron(name="AVDL"))
+          e.save()
+
+    `asserts_all_about` individually asserts each of the properties of the Neuron
+    including its existence. It does not recursively assert
+    properties of values set on the AVDL Neuron. If, for instance, the Neuron had a
+    *complex object* as the value for its receptor types with
+    information about the receptor's name primary agonist, etc., `asserts_all_about`
+      would say nothing about these. However, `asserts_all` (TODO)::
+
+          e.asserts_all(Neuron(name="AVDL",receptor=complex_receptor_object))
+
+    would make the aforementioned recursive statement.
+
+    Retrieving evidence
+    -------------------
+
+    .. Not tested with the latest
+
+    Retrieving evidence for an object is trivial as well ::
+
+          e = Evidence()
+          e.asserts(Connection(pre_cell="VA11", post_cell="VD12"))
+          for x in e.load():
+             print x
+
+    This would print all of the evidence for the connection between VA11 and VD12
+
+    It's important to note that the considerations of recursive evidence assertions
+    above do not operate for retrieval. Only evidence for the
+    particular object queried (the Connection in the example above), would be
+    returned and not any evidence for anything otherwise about VA11
+    or VD12.
 
     Attributes
     ----------
     asserts : ObjectProperty (value_type=DataObject)
-       Something asserted by this evidence
+       When used with an argument, state that this Evidence asserts that the
+       relationship is true.
+
+       Example::
+
+           import bibtex
+           bt = bibtex.parse("my.bib")
+           n1 = Neuron("AVAL")
+           n2 = Neuron("DA3")
+           c = Connection(pre=n1,post=n2,class="synapse")
+           e = Evidence(bibtex=bt['white86'])
+           e.asserts(c)
+
+       Other methods return objects which asserts accepts.
+
+       Example::
+
+           n1 = Neuron("AVAL")
+           r = n1.neighbor("DA3")
+           e = Evidence(bibtex=bt['white86'])
+           e.asserts(r)
+
+       When used without arguments, returns a sequence of statements asserted by
+       this evidence
+
+       Example::
+
+           import bibtex
+           bt = bibtex.parse("my.bib")
+           n1 = Neuron("AVAL")
+           n2 = Neuron("DA3")
+           c = Connection(pre=n1,post=n2,class="synapse")
+           e = Evidence(bibtex=bt['white86'])
+           e.asserts(c)
+           list(e.asserts()) # Returns a list [..., d, ...] such that d==c
     doi : DatatypeProperty
         A Digital Object Identifier (DOI) that provides evidence, optional
     pmid : DatatypeProperty
