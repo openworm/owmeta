5f92c11d9b883b3d45a3802368e1caf16b70f2ef
Stephen Larson <stephen.larson@gmail.com>
2015-06-19 13:01:09 -0700

    incrementing version, fixing urls

--- setup.py
+++ setup.py
@@ -19,14 +19,14 @@ PyOpenWorm
 ===========
 
 A unified, simple data access library in Python for data, facts, and models of
-*C. elegans* anatomy for the [OpenWorm project]http://www.openworm.org)
+*C. elegans* anatomy for the [OpenWorm project](http://www.openworm.org)
 
 What does it do?
 ----------------
 
 Enables a simple Python API for asking various questions about the cells of the
 *C. elegans*, enabling the sharing of data about *C. elegans* for the purpose
-of building a [data-to-model pipeline]http://docs.openworm.org/en/latest/projects
+of building a [data-to-model pipeline](http://docs.openworm.org/en/latest/projects
 for the OpenWorm project. In addition, it is a repository for various iterations
 of inferred / predicted data about *C. elegans*. Uncontroversial facts and
 inferred information are distinguished through the use of explicit Evidence
@@ -67,7 +67,7 @@ setup(
         'git://github.com/zopefoundation/ZODB.git#egg=ZODB',
     ],
     setup_requires = "six==1.9.0",
-    version = '0.5.2',
+    version = '0.5.3',
     packages = ['PyOpenWorm'],
     package_data = {
         'PyOpenWorm':['default.conf']
