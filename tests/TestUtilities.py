7db88a36c5b6892f5d0ea9ba976ea52d23eaafef
Gopal Sarma <gsarma.web@gmail.com>
2015-07-09 18:59:22 -0400

    Wrapped findSkippedTests into a function.

--- tests/TestUtilities.py
+++ tests/TestUtilities.py
@@ -1,23 +1,24 @@
 import os
 
-skippedTest = '@unittest.skip'
-expectedFailure = '@unittest.expectedFailure'
+def findSkippedTests():
+    skippedTest = '@unittest.skip'
+    expectedFailure = '@unittest.expectedFailure'
 
-# The boolean count is to make sure that multiple new lines aren't printed
-# in the event that multiple files have neither skipped tests nor
-# expected failures.
+    # The boolean count is to make sure that multiple new lines aren't printed
+    # in the event that multiple files have neither skipped tests nor
+    # expected failures.
 
-for fname in os.listdir('.'):
-    if os.path.isfile(fname) and fname[-3:] == ".py" and fname != 'TestUtilities.py':
-        with open(fname) as f:
-            count = False
-            for line in f:
-                if skippedTest in line:
-                    print 'found skipped test in file %s' %fname
-                    count = True
-                elif expectedFailure in line:
-                    print 'found expected failure in file %s' %fname
-                    count = True
-            if count:
-                print '\n'
+    for fname in os.listdir('.'):
+        if os.path.isfile(fname) and fname[-3:] == ".py" and fname != 'TestUtilities.py':
+            with open(fname) as f:
                 count = False
+                for line in f:
+                    if skippedTest in line:
+                        print 'found skipped test in file %s' %fname
+                        count = True
+                    elif expectedFailure in line:
+                        print 'found expected failure in file %s' %fname
+                        count = True
+                if count:
+                    print '\n'
+                    count = False
