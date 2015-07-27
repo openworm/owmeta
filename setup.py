77b7d9571618b9e002508df53678cddbc53c917b
Travis Jacobs <trav221@gmail.com>
2015-07-27 18:43:00 -0230

    Revert to using setup.py again

--- setup.py
+++ setup.py
@@ -61,7 +61,13 @@ setup(
         'ZConfig==3.0.4',
         'zdaemon==4.0.0',
         'zodb==4.1.0',
-        'zope.interface==4.1.1'
+        'zope.interface==4.1.1',
+        'FuXi'
+    ],
+    dependency_links = [
+        'git+https://github.com/NeuralEnsemble/libNeuroML.git#egg=libNeuroML'
+        'git+https://github.com/zopefoundation/ZODB.git#egg=ZODB',
+        'git+https://github.com/RDFLib/FuXi#egg=FuXi'
     ],
     version = '0.5.3',
     packages = ['PyOpenWorm'],
