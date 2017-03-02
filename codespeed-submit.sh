78ae93dca92eb21949e28e2ce51d51fef107dffd
Mark Watts <watts.mark2015@gmail.com>
2017-03-01 22:08:28 -0600

    Attempting to fix the codespeed submit branch on Travis-CI

--- codespeed-submit.sh
+++ codespeed-submit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BRANCH=${TRAVIS_BRANCH:-$(git symbolic-ref --short HEAD)}
+
 py.test $@ --cov=PyOpenWorm --code-speed-submit="https://owcs.pythonanywhere.com/" \
-    --environment="travis-ci" --branch="$(git symbolic-ref --short HEAD)" \
+    --environment="travis-ci" --branch="$BRANCH" \
     --commit="$(git rev-parse HEAD)" --password=${OWCS_KEY} --username=travisci ./tests/ProfileTest.py
