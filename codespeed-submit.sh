04d5a18e86887a349752b9b235fae0297fbf46fd
Mark Watts <watts.mark2015@gmail.com>
2017-03-01 22:31:38 -0600

    Added some echos to the codespeed submit script

--- codespeed-submit.sh
+++ codespeed-submit.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 BRANCH=${TRAVIS_BRANCH:-$(git symbolic-ref --short HEAD)}
+ENV=travis-ci
+COMMIT=$(git rev-parse HEAD)
+OWCS_USERNAME=travisci
+echo "Branch: $BRANCH"
+echo "Environment: $ENV"
+echo "Commit: $COMMIT"
+echo "User: $OWCS_USERNAME"
 
 py.test $@ --cov=PyOpenWorm --code-speed-submit="https://owcs.pythonanywhere.com/" \
-    --environment="travis-ci" --branch="$BRANCH" \
-    --commit="$(git rev-parse HEAD)" --password=${OWCS_KEY} --username=travisci ./tests/ProfileTest.py
+    --environment="$ENV" --branch="$BRANCH" --commit="$COMMIT" \
+    --password=${OWCS_KEY} --username=$OWCS_USERNAME ./tests/ProfileTest.py
