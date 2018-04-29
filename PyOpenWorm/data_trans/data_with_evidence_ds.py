d8e31a1cf71b34d078afb05a027dd8b3ea8db5a3
Mark Watts <watts.mark2015@gmail.com>
2018-04-29 09:37:18 -0500

    Adding utility for creating data contexts on the fly (#333)

--- PyOpenWorm/data_trans/data_with_evidence_ds.py
+++ PyOpenWorm/data_trans/data_with_evidence_ds.py
@@ -6,23 +6,40 @@ from .common_data import DS_NS
 
 
 class DataWithEvidenceDataSource(DataSource):
-    evidence_context = Informational(display_name='Evidence context',
-                                     description='The context in which evidence for the "Data context" is defined')
-    data_context = Informational(display_name='Data context',
-                                 description='The context in which primary data for this data source is defined')
-    contexts = Informational(display_name='Other contexts',
-                             description='Other contexts defined by the data translator')
+    evidence_context_info = Informational(display_name='Evidence context',
+                                          description='The context in which evidence for the "Data context" is defined')
+    data_context_info = Informational(display_name='Data context',
+                                      description='The context in which primary data for this data source is defined')
 
     rdf_namespace = Namespace(DS_NS['DataWithEvidenceDataSource#'])
 
     def __init__(self, *args, **kwargs):
         super(DataWithEvidenceDataSource, self).__init__(*args, **kwargs)
-        # print(self.translation._v)
+
         self.data_context = Context(ident=self.identifier + '-data',
                                     imported=(CONTEXT,))
         self.evidence_context = Context(ident=self.identifier + '-evidence',
                                         imported=(CONTEXT,))
+
+        self.data_context_info(self.data_context.rdf_object)
+        self.evidence_context_info(self.evidence_context.rdf_object)
+
         self.contexts = []
+        self.__ad_hoc_contexts = dict()
+
+    def data_context_for(self, **kwargs):
+        ctx = self.context_for(**kwargs)
+        self.data_context.add_import(ctx)
+        return ctx
+
+    def context_for(self, **kwargs):
+        key = "&".join(k + "=" + kwargs[k].identifier for k in sorted(kwargs.keys()))
+        res = self.__ad_hoc_contexts.get(key)
+        if res is None:
+            ctxid = self.identifier + '/context_for?' + key
+            self.__ad_hoc_contexts[key] = Context(ident=ctxid)
+            res = self.__ad_hoc_contexts[key]
+        return res
 
 
 __yarom_mapped_classes__ = (DataWithEvidenceDataSource,)
