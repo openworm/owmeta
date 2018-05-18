85e72040a7d8cef346183ab6e4c65a8fdaacab86
Mark Watts <watts.mark2015@gmail.com>
2018-05-18 17:01:13 -0500

    Upgrading pytest version
    - Needed for logfinish hook

--- tests/DocumentationTest.py
+++ tests/DocumentationTest.py
@@ -9,6 +9,7 @@ from .doctest_plugin import ALLOW_UNICODE, UnicodeOutputChecker
 
 doctest.OutputChecker = UnicodeOutputChecker
 
+
 class DocumentationTest(unittest.TestCase):
     def test_readme(self):
         [failure_count, return_count] = doctest.testfile("../README.md", optionflags=(ALLOW_UNICODE | doctest.ELLIPSIS))
