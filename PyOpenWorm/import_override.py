8b3478123bfca48c98658302e0e4cccc787a0023
Mark Watts <watts.mark2015@gmail.com>
2018-06-03 09:06:10 -0500

    Adding the missing header and exception message to excepthook

--- PyOpenWorm/import_override.py
+++ PyOpenWorm/import_override.py
@@ -85,10 +85,12 @@ class Overrider(object):
         import sys
 
         def filtering_except_hook(type, value, tb):
-            s = traceback.format_list([x for x in traceback.extract_tb(tb)
-                                       if x[2] != 'process_module' and
-                                       not x[0].endswith('PyOpenWorm/import_override.py') and
-                                       not (x[0].endswith('wrapt/wrappers.py') and x[2] == '__call__')])
+            s = ['Traceback (most recent call last, import_override removed):\n']
+            s += traceback.format_list([x for x in traceback.extract_tb(tb)
+                                        if x[2] != 'process_module' and
+                                        not x[0].endswith('PyOpenWorm/import_override.py') and
+                                        not (x[0].endswith('wrapt/wrappers.py') and x[2] == '__call__')])
+            s += traceback.format_exception_only(type, value)
             for l in s:
                 six.print_(l, file=sys.stderr, end='')
         sys.excepthook = filtering_except_hook
