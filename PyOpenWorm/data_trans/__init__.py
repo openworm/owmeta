e47a4c02b37c2e023f33ac3ad1996558386c6483
Mark Watts <watts.mark2015@gmail.com>
2018-06-10 10:16:41 -0500

    Adding somewhat more helpful documentation for data_trans

--- PyOpenWorm/data_trans/__init__.py
+++ PyOpenWorm/data_trans/__init__.py
@@ -1,1 +1,7 @@
-''' Adding for relative imports '''
+'''
+Data translators
+
+Some `DataSource` and `DataTranslator` types. Some are deal with generic file types (e.g., comma-separated values) while
+others are specific to the format of a kind of file housed in PyOpenWorm.
+
+'''
