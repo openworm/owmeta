116c551cb038dfd8f8c8c342f57fb1343ca2bb98
Mark Watts <watts.mark2015@gmail.com>
2018-06-16 06:18:53 -0500

    Filling out git repo provider impl (#350)

--- PyOpenWorm/git_repo.py
+++ PyOpenWorm/git_repo.py
@@ -5,6 +5,25 @@ class GitRepoProvider(object):
 
     def __init__(self):
         self._repo = None
+        self.base = None
 
     def init(self, base=None):
+        base = self.base if not base else base
         self._repo = Repo.init(base)
+
+    def add(self, files):
+        self.repo().index.add(files)
+
+    def remove(self, files, recursive=False):
+        self.repo().index.remove(files, r=recursive)
+
+    def reset(self):
+        self.repo().index.reset()
+
+    def commit(self, msg):
+        self.repo().index.commit(msg)
+
+    def repo(self):
+        if self._repo is None:
+            self._repo = Repo(self.base)
+        return self._repo
