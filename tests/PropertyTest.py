46b44bd9a4ba33ba05686240490ae86099e3c5f7
Vuillemot Florian <vuillemot.florian@gmail.com>
2018-07-15 17:19:08 +0200

    Add test on Property class

--- tests/PropertyTest.py
+++ tests/PropertyTest.py
@@ -6,15 +6,46 @@ from .DataTestTemplate import _DataTest
 class PropertyTest(_DataTest):
     def test_one(self):
         """ `one` should return None if there isn't a value or just the value if there is one """
-        class T(Property):
-            def __init__(self):
-                Property.__init__(self)
-                self.b = False
-
-            def get(self):
-                if self.b:
-                    yield "12"
         t = T()
         self.assertIsNone(t.one())
-        t.b=True
-        self.assertEqual('12', t.one())
+        t.b = 42
+        self.assertEqual(42, t.one())
+
+    def test_context_getter(self):
+        p = Property()
+        self.assertIsNone(p.context)
+
+    def test_context_setter(self):
+        p = Property()
+        p.context = 42
+        self.assertEquals(p.context, 42)
+
+    def test_values_property(self):
+        p = Property()
+        self.assertEquals(p.values, [])
+
+    def test_call_without_arg(self):
+        t = T()
+        self.assertIsNone(t())
+        t.b = 42
+        self.assertEqual(42, t())
+
+    def test_call_with_arg(self):
+        t = T()
+        self.assertIsNone(t())
+        n = t(42)
+        self.assertEquals(n.b, 42)
+
+
+class T(Property):
+    def __init__(self):
+        Property.__init__(self)
+        self.b = None
+
+    def get(self):
+        if self.b is not None:
+            yield self.b
+        return self.b
+
+    def set(self, b):
+        self.b = b
