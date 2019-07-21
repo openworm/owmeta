2e490a439cf43ae5fbad940d3465831404823832
Mark Watts <watts.mark2015@gmail.com>
2019-07-21 15:37:44 -0500

    Adjusting for incompatability introduced in docutils (dependency of sphinx)

--- setup.py
+++ setup.py
@@ -79,7 +79,8 @@ setup(
       + (['Sphinx<1.8.4'] if PY34 or PY2 else [])
       + (['backports.tempfile==1.0'] if PY2 else [])
       + (['Jinja2<2.9'] if PY34 else [])
-      + (['scandir'] if PY2 or PY34 else []),
+      + (['scandir'] if PY2 or PY34 else [])
+      + (['docutils<0.15'] if PY2 else []),
     version=version,
     packages=['PyOpenWorm',
               'PyOpenWorm.data_trans'],
