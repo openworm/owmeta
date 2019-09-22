c2635e42a888508a64f60221827b3d59a1e3e7e4
Mark Watts <watts.mark2015@gmail.com>
2019-09-22 09:09:18 -0500

    Fixing ci-install.sh test variables

--- ci-install.sh
+++ ci-install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -ex
 
 TARGET=.
-if [ "$MYSQL_SOURCE" ] ; then
+if [ "$MYSQL_TEST" ] ; then
     TARGET=".[mysql_source_mysql_connector] .[mysql_source_mysqlclient]"
 fi
 
-if [ "$POSTGRES_SOURCE" ] ; then
+if [ "$POSTGRES_TEST" ] ; then
     TARGET=".[postgres_source_psycopg] .[postgres_source_pg8000]"
 fi
 
