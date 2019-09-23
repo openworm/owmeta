dbdb1ea60d91b9dc7234e91b8b585f05be4ce8f9
Mark Watts <watts.mark2015@gmail.com>
2019-09-23 08:12:07 -0500

    Fixing target for ci-install.sh

--- ci-install.sh
+++ ci-install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -ex
 
-TARGET=.
+TARGET=
 if [ "$MYSQL_TEST" ] ; then
     TARGET=".[mysql_source_mysql_connector] .[mysql_source_mysqlclient]"
 fi
 
 if [ "$POSTGRES_TEST" ] ; then
-    TARGET=".[postgres_source_psycopg] .[postgres_source_pg8000]"
+    TARGET="$TARGET .[postgres_source_psycopg] .[postgres_source_pg8000]"
 fi
 
-pip install $TARGET
+pip install ${TARGET:-"."}
