38ddd63f7ab5688e6b5ab80d4e5b35b1b3c58281
Mark Watts <watts.mark2015@gmail.com>
2020-01-31 06:16:47 -0600

    Moving end of owm CLI profiling to after output
    - Catches execution that happens in output generators

--- owmeta/cli.py
+++ owmeta/cli.py
@@ -120,9 +120,6 @@ def main():
             # In case someone forgets to add a helpful message for their user error
             s = 'Received error: ' + FCN(type(e))
         die(s)
-    if environ.get('OWM_CLI_PROFILE'):
-        profiler.disable()
-        profiler.dump_stats(environ['OWM_CLI_PROFILE'])
     output_mode = ns_handler.output_mode
     text_field_separator = ns_handler.text_field_separator
     text_record_separator = ns_handler.text_record_separator
@@ -216,6 +213,10 @@ def main():
                             print(out.text_format(x), end=text_record_separator)
                         else:
                             print(x, end=text_record_separator)
+
+    if environ.get('OWM_CLI_PROFILE'):
+        profiler.disable()
+        profiler.dump_stats(environ['OWM_CLI_PROFILE'])
 
 
 if __name__ == '__main__':
