1ef837f09ccb88bdbcac69f62a783b8997505483
Mark Watts <watts.mark2015@gmail.com>
2020-02-06 12:01:59 -0600

    Deleting setup for travis CI belonging to owmeta

--- ci-test.sh
+++ ci-test.sh
@@ -19,48 +19,14 @@ list_coverage () {
 
 cleanup_coverage () {
     if [ "$COVERAGES" ] ; then
-        rm $COVERAGES
+        rm -f $COVERAGES
     fi
 }
 
 trap cleanup_coverage EXIT
 
-if [ "$SQLITE_TEST" ] ; then
-    pt --verbose -m sqlite_source
-    add_coverage
-fi
-
-if [ "$POSTGRES_TEST" ] ; then
-    psql -c 'DROP DATABASE IF EXISTS test;' -U postgres
-    psql -c 'create database test;' -U postgres
-    export POSTGRES_URI='postgresql+psycopg2://postgres@localhost/test'
-    pt --verbose -m postgres_source
-    add_coverage
-    export POSTGRES_URI='postgresql+pg8000://postgres@localhost/test'
-    pt --verbose -m postgres_source
-    add_coverage
-fi
-
-if [ "$MYSQL_TEST" ] ; then
-    mysql -u root -e 'DROP DATABASE IF EXISTS test;'
-    mysql -u root -e 'CREATE DATABASE test DEFAULT CHARACTER SET utf8;'
-    mysql -u root -e "CREATE USER IF NOT EXISTS 'test' IDENTIFIED BY 'password';"
-    mysql -u root -e "GRANT ALL ON test.* TO 'test';"
-    export MYSQL_URI='mysql+mysqlconnector://test:password@localhost/test?charset=utf8&auth_plugin=mysql_native_password'
-    pt --verbose -m mysql_source
-    add_coverage
-    export MYSQL_URI='mysql+mysqldb://test:password@localhost/test?charset=utf8'
-    pt --verbose -m mysql_source
-    add_coverage
-fi
-pt --verbose -m "'not inttest and not owm_cli_test'"
+pt --verbose -m "'not inttest'"
 add_coverage
 pt --verbose -m inttest
-add_coverage
-if [ $WORKERS ] ; then
-    pt --workers $WORKERS -m owm_cli_test
-else
-    pt --verbose -m owm_cli_test
-fi
 add_coverage
 coverage combine $(list_coverage)
