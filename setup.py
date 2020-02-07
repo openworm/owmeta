31759e9fe69e370fac6a456e59eaf87227e94484
Mark Watts <watts.mark2015@gmail.com>
2020-02-06 20:37:10 -0600

    Removing package dependencies belonging only to owmeta-core

--- setup.py
+++ setup.py
@@ -30,15 +30,6 @@ for line in open('owmeta/__init__.py'):
     if line.startswith("__version__"):
         version = line.split("=")[1].strip()[1:-1]
 
-package_data_excludes = ['.*', '*.bkp', '~*']
-
-
-def excludes(base):
-    res = []
-    for x in package_data_excludes:
-        res.append(os.path.join(base, x))
-    return res
-
 
 setup(
     name='owmeta',
@@ -54,43 +45,11 @@ setup(
     install_requires=[
         'owmeta-core',
         'bibtexparser~=1.1.0',
-        'BTrees>=4.0.8',
-        'gitpython>=2.1.1',
-        'lazy-object-proxy==1.2.1',
         'libneuroml',
         'numpydoc>=0.7.0',
-        'persistent>=4.0.8',
-        'Pint==0.8.1',
-        'pow-store-zodb==0.0.7',
         'rdflib>=4.1.2',
-        'six~=1.10',
-        'tqdm~=4.23',
-        'termcolor==1.1.0',
-        'transaction>=1.4.4',
-        'wrapt~=1.11.1',
-        'yarom~=0.12.0.dev0',
-        'zc.lockfile',
-        'ZConfig==3.0.4',
-        'zdaemon==4.0.0',
-        'zodb>=4.1.0',
-        'rdflib-sqlalchemy~=0.4.0',
-        'pyyaml',
+        'six~=1.10'
     ],
-    extras_require={
-        # SQL source support
-        'mysql_source_mysql_connector': [
-            'mysql-connector-python'
-        ],
-        'mysql_source_mysqlclient': [
-            'mysqlclient'
-        ],
-        'postgres_source_psycopg': [
-            'psycopg2'
-        ],
-        'postgres_source_pg8000': [
-            'pg8000'
-        ]
-    },
     version=version,
     packages=['owmeta',
               'owmeta.data_trans',
@@ -109,7 +68,6 @@ setup(
         ],
         'owmeta_core.cli_hints': 'hints = owmeta.cli_hints:CLI_HINTS',
     },
-    package_data={'owmeta': ['default.conf']},
     classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
