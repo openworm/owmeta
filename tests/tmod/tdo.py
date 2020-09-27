9130d8092c7e7e6d6ed5ca47c459ebd855daf001
Mark Watts <watts.mark2015@gmail.com>
2020-09-27 17:32:04 -0500

    Removing unused test files

--- tests/tmod/tdo.py
+++
@@ -1,8 +0,0 @@
-from owmeta_core.mapper import mapped
-from owmeta_core.dataobject import DataObject, DatatypeProperty
-
-
-@mapped
-class TDO(DataObject):
-    rdf_type = 'http://openworm.org/entities/TDO'
-    a = DatatypeProperty()
