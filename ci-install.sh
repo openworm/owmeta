27c7e4b3eb20b2d4dfde3f6b1ede1e030cb1a24a
Mark Watts <watts.mark2015@gmail.com>
2021-11-28 10:38:53 -0600

    Adding a constraint for pyparsing ver on py 3.6
    - Also adding 3.9 and 3.10 classifiers to project metadata

--- ci-install.sh
+++ ci-install.sh
@@ -10,3 +10,16 @@ if [ $BUNDLE_TESTS ] ; then
     owm bundle install openworm/owmeta-schema
     owm bundle install openworm/owmeta-data
 fi
+if [ "$PYTHON_VERSION" == "3.6" ] ; then
+    # pyparsing 3.0 no longer allows setting the .name attribute for
+    # TokenConverter, but the fix for that only goes in RDFLib 6 which
+    # does not support Python 3.6. See
+    # https://github.com/RDFLib/rdflib/issues/1190 and
+    # https://github.com/rdflib/rdflib/issues/1370 for details on the issue.
+    #
+    # Since we're a library, it's up to the user how to resolve this
+    # (e.g., ignore and do not use SPARQL) so we do not impose any
+    # version constraints in package metadata, just adjust so the build
+    # passes
+    pip install 'pyparsing<3.0.0'
+fi
