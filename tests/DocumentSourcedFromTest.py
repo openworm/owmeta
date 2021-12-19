1f410b97e43ec5b903dca8654d1458c719e25af4
Mark Watts <watts.mark2015@gmail.com>
2021-12-18 18:18:05 -0600

    Adding a test for SourcedFrom

--- tests/DocumentSourcedFromTest.py
+++ tests/DocumentSourcedFromTest.py
@@ -7,6 +7,8 @@ from rdflib.namespace import Namespace
 from owmeta_core.datasource import DataSource
 from owmeta_pytest_plugin import bundle_versions
 
+from owmeta.document import SourcedFrom
+
 
 EX = Namespace('http://example.org/')
 
@@ -25,21 +27,17 @@ def test_augment_cmd_load(owmeta_schema_bundle, owm_project):
             'tests/test_modules/evidence_ds_mixin_source.py')
     owm_project.sh('owm save test_module.asource')
 
-    with owm_project.owm().connect() as conn:
-        conn.rdf.namespace_manager.bind('ex', EX)
-
+    owm_project.sh(f'owm namespace bind ex {EX}')
+    ctxid = owm.default_context.identifier
     owm_project.sh(
-            'owm declare owmeta.document:Document doi=10.1101/398370 --id=ex:doc')
-    with owm_project.owm().connect() as conn:
-        print(conn.rdf.serialize(format='n3').decode('utf-8'))
-    owm_project.sh(
-            'owm declare owmeta.evidence:Evidence reference=ex:doc'
-            '    rdfs_comment="This says some stuff" --id=ex:ev',
-            'owm declare test_module.asource:ASource a="Some arbitrary value" --id=ex:ds',
-            'owm source attach-evidence ex:ds ex:ev')
+            f'owm contexts add-import {ctxid} http://schema.openworm.org/2020/07/sci',
+            'owm declare owmeta.document:Document doi=10.1101/398370 --id=ex:doc',
+            'owm declare test_module.asource:ASource'
+            ' owmeta.document:SourcedFrom=ex:doc'
+            ' --id=ex:ds',)
 
     owm = owm_project.owm()
     with owm.connect():
-        ds = list(owm.default_context(DataSource)(ident=EX['ds']).load())
-        ev = ds.attached_evidence.one()
-        assert ev.identifier == EX['ev']
+        ds = owm.default_context.stored(DataSource)(ident=EX['ds']).load_one()
+        doc = ds.attach_property(SourcedFrom).one()
+        assert doc.identifier == EX['doc']
